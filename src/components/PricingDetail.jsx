ad9f9b5499bf7d514fc45fe97e7bbdfe41622c52
jhonatanquirama <quiramapino47@gmail.com>
2020-08-20 15:33:17 -0500

    actualizaciones5

--- src/components/PricingDetail.jsx
+++ src/components/PricingDetail.jsx
@@ -1,25 +1,26 @@
 import React from 'react'
 
-const PricingDetail = (props) => (
+const PricingDetail = ({plan,precio,items,textoButton}) => (
 <div className='card mb-4 shadow-sm'>
           <div className='card-header'>
-<h4 className='my-0 font-weight-normal'>{props.plan}</h4>
+<h4 className='my-0 font-weight-normal'>{plan}</h4>
           </div>
           <div className='card-body'>
             <h1 className='card-title pricing-card-title'>
-              {props.precio} <small className='text-muted'>/ mo</small>
+              {precio} <small className='text-muted'>/ mo</small>
             </h1>
             <ul className='list-unstyled mt-3 mb-4'>
-                {props.items.map((item, index) => (<li key={index}>{item}</li>))}
-            </ul>
+                {items.map((item, index) => (<li key={index}>{item}</li>))}
+               </ul>
             <button
               type='button'
               className='btn btn-lg btn-block btn-outline-primary'
             >
-             {props.textoButton}
+             {textoButton}
             </button>
           </div>
-        </div>
+        </div> 
 );
+
 
 export default PricingDetail;
